dc4d66ca9ac2ed30fe81dfc2e27429bd92da1ea2
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-26 15:48:41 -0300

    not found exception class

--- src/main/java/br/com/ds/examgenerator/endpoint/v1/ProfessorEndPoint.java
+++ src/main/java/br/com/ds/examgenerator/endpoint/v1/ProfessorEndPoint.java
@@ -1,5 +1,6 @@
 package br.com.ds.examgenerator.endpoint.v1;
 
+import br.com.ds.examgenerator.error.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,13 @@ public class ProfessorEndPoint {
 
     @GetMapping
     public ResponseEntity<?> hi() {
+//        verifyIfProfessorExists();
         return new ResponseEntity<>("Hi", HttpStatus.OK);
+    }
+
+    public void verifyIfProfessorExists(Long id) {
+        if (id == null)
+            throw new ResourceNotFoundException("Professor not found for ID: " + id);
     }
 
 }
--- src/main/java/br/com/ds/examgenerator/error/ResourceNotFoundException.java
+++ src/main/java/br/com/ds/examgenerator/error/ResourceNotFoundException.java
@@ -1,7 +1,14 @@
 package br.com.ds.examgenerator.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * @author Douglas Souza on 26/03/2019
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
