1606d989fa27ce63eff1906cb2346470529ade06
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-27 10:29:24 -0300

    JWT authorization allowed

--- src/main/java/br/com/ds/examgenerator/persistence/model/ApplicationUser.java
+++ src/main/java/br/com/ds/examgenerator/persistence/model/ApplicationUser.java
@@ -24,4 +24,12 @@ public class ApplicationUser extends AbstractEntity {
     @OneToOne
     private Professor professor;
 
+    public ApplicationUser() {
+    }
+
+    public ApplicationUser(ApplicationUser applicationUser) {
+        this.username = applicationUser.username;
+        this.password = applicationUser.password;
+        this.professor = applicationUser.professor;
+    }
 }
--- src/main/java/br/com/ds/examgenerator/security/service/CustomUserDetailsService.java
+++ src/main/java/br/com/ds/examgenerator/security/service/CustomUserDetailsService.java
@@ -3,11 +3,15 @@ package br.com.ds.examgenerator.security.service;
 import br.com.ds.examgenerator.persistence.model.ApplicationUser;
 import br.com.ds.examgenerator.persistence.repository.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -26,7 +30,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         ApplicationUser applicationUser = loadApplicationUserByUserName(username);
-        return
+        return new CustomUserDetails(applicationUser);
     }
 
     public ApplicationUser loadApplicationUserByUserName(String username) {
@@ -35,6 +39,36 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("ApplicationUser not found"));
     }
 
-    private static final class CustomUserDetails extends ApplicationUser implements UserDetails {
+    private final static class CustomUserDetails extends ApplicationUser implements UserDetails {
+        public CustomUserDetails(ApplicationUser applicationUser) {
+            super(applicationUser);
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            List<GrantedAuthority> authorityListProfessor = AuthorityUtils.createAuthorityList("ROLE_PROFESSOR");
+            List<GrantedAuthority> authorityListStudent = AuthorityUtils.createAuthorityList("ROLE_STUDENT");
+            return this.getProfessor() != null ? authorityListProfessor : authorityListStudent;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+           return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
     }
 }
