aff76520e1dcb906df2f24a5906686df04250a44
Douglas Souza <douglas_souza03@hotmail.com>
2019-03-29 14:24:18 -0300

    security config created

--- src/main/java/br/com/ds/examgenerator/security/config/SecurityConfig.java
+++ src/main/java/br/com/ds/examgenerator/security/config/SecurityConfig.java
@@ -1,7 +1,41 @@
 package br.com.ds.examgenerator.security.config;
 
+import br.com.ds.examgenerator.security.filter.JWTAuthenticationFilter;
+import br.com.ds.examgenerator.security.filter.JWTAuthorizationFilter;
+import br.com.ds.examgenerator.security.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+
 /**
  * @author Douglas Souza on 29/03/2019
  */
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*/professor/**").hasRole("PROFESSOR")
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), customUserDetailsService));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
--- src/main/java/br/com/ds/examgenerator/security/filter/Constants.java
+++ src/main/java/br/com/ds/examgenerator/security/filter/Constants.java
@@ -1,10 +1,14 @@
 package br.com.ds.examgenerator.security.filter;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Douglas Souza on 25/03/2019
  */
+@Slf4j
 public class Constants {
     public static final String SECRET = "secre";
     public static final String TOKEN_PREFIX = "Bearer ";
@@ -12,6 +16,7 @@ public class Constants {
     public static final long EXPIRATION_TIME = 86400000L; //1 day
 
     public static void main(String[] args) {
-        System.out.println(TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+        log.info("Time Unit Days: " + TimeUnit.MILLISECONDS.convert(1, TimeUnit.DAYS));
+        log.info("Encrypt Pass: " + new BCryptPasswordEncoder().encode("spring"));
     }
 }
